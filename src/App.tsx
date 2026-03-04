ab0c7fb427fc04135a0952b0e043ec5dd0fd2789
Evg3n22 <evgen.gulevaty@gmail.com>
2026-03-04 12:12:31 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+type SelectedData = 'getAll' | 'get5First' | 'getRedGoods' | '';
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [goodsList, setGoodsList] = useState<Good[]>([]);
+  const [selectedData, setSelectedData] = useState<SelectedData>('');
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  useEffect(() => {
+    if (!selectedData) {
+      return;
+    }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    if (selectedData === 'getAll') {
+      getAll().then(setGoodsList);
+    }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    if (selectedData === 'get5First') {
+      get5First().then(setGoodsList);
+    }
+
+    if (selectedData === 'getRedGoods') {
+      getRedGoods().then(setGoodsList);
+    }
+  }, [selectedData]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setSelectedData('getAll')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setSelectedData('get5First')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setSelectedData('getRedGoods')}
+      >
+        Load red goods
+      </button>
+
+      {selectedData && <GoodsList goods={goodsList} />}
+    </div>
+  );
+};
